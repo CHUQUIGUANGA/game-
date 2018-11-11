e89e46b22a7cc28f8c5d89ddcc3f2a75eed63fd3
DIEGO CHUQUIGUANGA <44952885+CHUQUIGUANGA@users.noreply.github.com>
2018-11-11 23:25:08 +0100

    Update DiegoChuquiguanga.java

--- DiegoChuquiguanga.java
+++ DiegoChuquiguanga.java
@@ -1,20 +1,4 @@
-/*
- *1 Se pedirá por teclado el nivel de dificultad deseado en el juego,
- *el rango de dificultad estará comprendido entre 1.‐ muy fácil, hasta. ‐ 5 imposible
- *
- *2. Se generará un número aleatorio comprendido entre 1 y el valor máximo (incluidos).  
- *El valor máximo dependerá del nivel de dificultad, según la tabla:
- *
- *3. Seguidamente se pedirán números al usuario, hasta que adivine el número 
- *aleatorio, contabilizando el número de veces que ha errado.
- *
- *4. La puntuación se calculará según la siguiente tabla, calculando el tanto por
- *ciento que representa el número de errores cometidos, según el valor máximo del número máximo.
- *
- *5. Se mostrará la puntuación acumulada (si ha jugado más numeritos)
- *6. Se le preguntará si quiere volver a jugar (0 .‐ vuelve a jugar, otra cosa finaliza el programa)
- *7. Si desea volver a jugar, se empezará desde el punto 1.
-*/
+
 package diegochuquiguanga;
 
 import javax.swing.JOptionPane;
